06498da3cf826cd905481822cc2c68824dce83d4
omar1749004 <mhsnmr68@gmail.com>
2025-05-04 16:30:47 +0300

    create splash view

---
+++ lib/features/splash/presenttaion/views/splash_view_body.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class SpashViewBody extends StatelessWidget {
+  const SpashViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        
+      ],
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:bookly_clean_arc/features/splash/presenttaion/views/splash_view.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
+
+void main() {
+  runApp(const BooklyApp());
+}
+
+class BooklyApp extends StatelessWidget {
+  const BooklyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      home: SplashView(),
+    );
+  }
+}
